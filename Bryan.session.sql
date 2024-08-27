7df5a84e8545a4f3899757d017d46e6cb4dac48d
bryanpintado <bryanp.0311@gmail.com>
2024-08-27 19:48:41 -0400

    added queries

--- Bryan.session.sql
+++ Bryan.session.sql
@@ -1,2 +1,15 @@
 select *
+from retail_orders -- find top 10 highest revenue generating products
+select product_id,
+    sum(sale_price) as sales
 from retail_orders
+GROUP BY product_id
+order by sales desc -- find top 5 highest selling products in each region
+select region,
+    product_id,
+    sum(sale_price)
+from retail_orders
+group by region,
+    product_id
+order by region,
+    sum(sale_price) desc
