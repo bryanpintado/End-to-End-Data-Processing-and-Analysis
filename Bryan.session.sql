25d642ce728e1c8c8382a63b42c4fbeffe48e3a0
bryanpintado <bryanp.0311@gmail.com>
2024-08-28 15:43:33 -0400

    added queries

--- Bryan.session.sql
+++ Bryan.session.sql
@@ -12,4 +12,20 @@ from retail_orders
 group by region,
     product_id
 order by region,
-    sum(sale_price) desc
+    sum(sale_price) desc -- find month over montyh growth comparison for 2022 and 2023 sales
+select year(order_date) as year,
+    month(order_date) as month,
+    sum(sale_price) as sales
+from retail_orders
+group by year,
+    month
+order by month,
+    year desc -- for each category which month had the highest sales
+select category,
+    sum(sale_price) as sales,
+    month(order_date) as month
+from retail_orders
+group by category,
+    month
+order by category,
+    sales desc
